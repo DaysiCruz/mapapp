7800c2b4b75cf192c5d1072db5adc55aad5c026e
Daysi Cruz <daysicruz960@gmail.com>
2024-07-15 20:26:29 -0600

    Agregando Bootstrap al proyecto

--- js/map.js
+++ js/map.js
@@ -6,28 +6,29 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 6,
         center: {lat: 13.705243, lng: -89.24231}
     });
 
-    var clients = [
-        {name: 'Punto 1', lat: 13.705243, lng: -89.24231},
-        {name: 'Punto 2', lat: 13.696674, lng: -89.197927},
-        {name: 'Punto 3', lat: 14.692511, lng: -87.86136},
-        {name: 'Punto 4', lat: 12.022747, lng: -86.252007},
-        {name: 'Punto 5', lat: 8.103289, lng: -80.596013}
+    const clients = [
+        {name: 'Daniel Chavez', lat: 13.705243, lng: -89.24231},
+        {name: 'Paola Alvarenga', lat: 13.696674, lng: -89.197927},
+        {name: 'Daniela Paz', lat: 14.692511, lng: -87.86136},
+        {name: 'John Smith', lat: 12.022747, lng: -86.252007},
+        {name: 'Ximena Alfaro', lat: 8.103289, lng: -80.596013}
     ];
 
     clients.forEach(function(client) {
-        var marker = new google.maps.Marker({
+        const marker = new google.maps.Marker({
             position: {lat: client.lat, lng: client.lng},
             map: map,
             title: client.name
         });
 
-        var infoWindow = new google.maps.InfoWindow({
-            content: client.name
+        const infoWindow = new google.maps.InfoWindow({
+            content: 
+                client.name,
         });
 
         marker.addListener('click', function() {
